a992b75667286b5e86ace8e20612c1f729ab709e
Dinoosauro <80783030+Dinoosauro@users.noreply.github.com>
2023-08-22 22:17:57 +0200

    Quick fixes in Webpack deploy
    - Italian translation now are uploaded
    - Fixed some issues in CSS

--- webpack.config.js
+++ webpack.config.js
@@ -8,30 +8,30 @@ const cleanCss = require("clean-css");
 if (fs.existsSync("dist/")) fs.rmdirSync("dist/", { recursive: true, force: true });
 fs.mkdirSync("dist");
 module.exports = {
-    entry: "./script.js",
-    output: {
-      filename: 'out.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    mode: 'production',
-    optimization: {
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            keep_fnames: true,
-          }
-        })
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'index.html',
-        path: path.resolve(__dirname, 'dist')
+  entry: "./script.js",
+  output: {
+    filename: 'out.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_fnames: true,
+        }
       })
     ]
-  }
-new cleanCss({ returnPromise: true }).minify(fs.readFileSync("./style.css")).then((res) => {fs.writeFileSync("dist/style.css", res.styles)});
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      path: path.resolve(__dirname, 'dist')
+    })
+  ]
+}
+new cleanCss({ returnPromise: true }).minify(fs.readFileSync("./style.css")).then((res) => { fs.writeFileSync("dist/style.css", res.styles) });
 let copyAssets = [
   'assets/mergedContent.json',
   'assets/icon.png',
@@ -41,13 +41,14 @@ let copyAssets = [
   'canvas2svg.js',
   'manifest.json',
   'assets/FirstClip.mp4',
-    'assets/SecondClip.mp4',
-    'assets/ThirdClip.mp4',
-    'assets/FirstZoomOption.mp4',
-    'assets/SecondZoomOption.mp4',
-    'service-worker.js',
+  'assets/SecondClip.mp4',
+  'assets/ThirdClip.mp4',
+  'assets/FirstZoomOption.mp4',
+  'assets/SecondZoomOption.mp4',
+  'service-worker.js',
+  'translationItems/it.json'
 ]
 for (let item of copyAssets) {
-    if (item.indexOf("/") !== -1 && !fs.existsSync(`dist/${item.substring(0, item.lastIndexOf("/"))}`)) fs.mkdirSync(`dist/${item.substring(0, item.lastIndexOf("/"))}`);
-    fs.copyFileSync(item, `dist/${item}`);
+  if (item.indexOf("/") !== -1 && !fs.existsSync(`dist/${item.substring(0, item.lastIndexOf("/"))}`)) fs.mkdirSync(`dist/${item.substring(0, item.lastIndexOf("/"))}`);
+  fs.copyFileSync(item, `dist/${item}`);
 }
