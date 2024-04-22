7fe9d43d6d2761588e203bda8596c8fe1a21268d
Dinoosauro <80783030+Dinoosauro@users.noreply.github.com>
2024-04-22 23:07:10 +0200

    Added documentation to RerenderButtons

--- src/Scripts/RerenderButtons.ts
+++ src/Scripts/RerenderButtons.ts
@@ -1,8 +1,18 @@
 const map = new Map<string, React.Dispatch<React.SetStateAction<boolean>>>([]);
 export default {
+    /**
+     * Store the current state of the toolbar button, so that it can be called later
+     * @param state the callback that'll permit to update the state of this button
+     * @param id the identifier of what that button does
+     */
     set: (state: React.Dispatch<React.SetStateAction<boolean>>, id: string) => {
         map.set(id, state);
     },
+    /**
+     * Change the state of a circular button (setting it enabled or disabled)
+     * @param id the identifier of the button to change
+     * @param force the new value of the state. If undefined, the opposite of the current value will be used.
+     */
     update: (id: string, force?: boolean) => {
         const item = map.get(id);
         console.log(id, item);
